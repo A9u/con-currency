575e4f2b0c6b6e515cb858abe79bb7dcbd394d37
Anusha Bhat <anusha@joshsoftware.com>
2020-05-07 13:14:29 +0530

    Code Refactoring

--- service/startprocess.go
+++ service/startprocess.go
@@ -8,42 +8,27 @@ import (
 	logger "github.com/sirupsen/logrus"
 )
 
-// type XEServiceMock struct {
-// 	URL      string
-// 	Username string
-// 	Password string
-// }
-
-// func (xeService XEServiceMock) GetConverte(currency string) (xeResp model.XEcurrency, err error) {
-// 	return
-// }
-
 //StartProcess start the process of fetching currency exchange rates and insert it into database
 func StartProcess(currencies []string, converter exchangerate.Converter, storer db.Storer) {
 	var rowsAffected int64
 
-	//xe := XEServiceMock{}
-	// creating channel for sending jobs
-	jobs := make(chan string, len(currencies))
+	// creating channel for sending currency
+	currencyChan := make(chan string)
 
-	// creating channel for recieving errors and response
-	results := make(chan model.Results, len(currencies))
-
-	// Creating workers
-	for w := 0; w <= 10; w++ {
-		go processCurrencies(converter, storer, jobs, results)
-	}
+	// creating channel for receiving errors and response
+	resultChan := make(chan model.Results)
 
 	// sending jobs
 	for _, currency := range currencies {
-		jobs <- currency
+		go processCurrency(converter, storer, currencyChan, resultChan)
+		currencyChan <- currency
 	}
 
-	close(jobs)
+	close(currencyChan)
 
 	// recieving results
 	for i := 0; i < len(currencies); i++ {
-		res := <-results
+		res := <-resultChan
 		if res.Err != nil {
 			logger.WithField("err", res.Err.Error()).Error("Exit")
 			return
@@ -53,40 +38,24 @@ func StartProcess(currencies []string, converter exchangerate.Converter, storer 
 	}
 
 	logger.WithField("rows affected", rowsAffected).Info("Job successfull")
-}
-
-// func processCurrencies(xeService xeservice.GetConverter, dbInstance *sql.DB, jobs <-chan string, results chan<- model.Results) {
-func processCurrencies(converter exchangerate.Converter, storer db.Storer, jobs <-chan string, results chan<- model.Results) {
-
-	for currency := range jobs {
-		rowCnt, err := processCurrency(currency, converter, storer)
-		if err != nil {
-			results <- model.Results{
-				RowsAffected: 0,
-				Err:          err,
-			}
-			return
-		}
-
-		results <- model.Results{
-			RowsAffected: rowCnt,
-			Err:          nil,
-		}
-
-	}
+	close(resultChan)
 }
 
 // func processCurrency(currency string, xeService xeservice.GetConverter, dbInstance *sql.DB) (rowCnt int64, err error) {
-func processCurrency(currency string, converter exchangerate.Converter, storer db.Storer) (rowCnt int64, err error) {
+func processCurrency(converter exchangerate.Converter, storer db.Storer, currencyChan <-chan string, results chan<- model.Results) {
+	currency := <-currencyChan
 	resp, err := converter.Get(currency)
 	if err != nil {
 		return
 	}
 
-	rowCnt, err = storer.UpsertCurrencies(resp)
+	rowCnt, err := storer.UpsertCurrencies(resp)
 	if err != nil {
 		return
 	}
 
-	return
+	results <- model.Results{
+		RowsAffected: rowCnt,
+		Err:          nil,
+	}
 }
